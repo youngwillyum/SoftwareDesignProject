806f5f0944c4859b67f73a4da30784062bbe05ca
youngwillyum <31012583+youngwillyum@users.noreply.github.com>
2022-11-16 16:43:56 -0600

    Initial Push

--- Server/index.js
+++ Server/index.js
@@ -15,4 +15,10 @@ MongoClient.connect(
 )
 .catch (err => {
     console.error(err.stack)
+    process.exit(1);
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log('listening on port ${port}')
+    })
 })
