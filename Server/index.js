041c0e0fbbac53d0667387925219a8ccbf418b6a
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2022-11-29 21:01:15 -0600

    Server now returns tables

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,12 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import ResturantsDAO from "./dao/restaurantsDAO.js"
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import ResturantsDAO from "./dao/restaurantsDAO.js";
+import TablesDAO from "./dao/tablesDAO.js";
+
+
 dotenv.config()
 const MongoClient = mongodb.MongoClient
-
 const port = process.env.PORT || 8000
 
 MongoClient.connect(
@@ -19,6 +21,7 @@ MongoClient.connect(
     process.exit(1);
 })
 .then(async client => {
+    await TablesDAO.injectDB(client);
     await ResturantsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listening on port ${port}`)
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import resturants from "./api/restaurants.route.js"
+import tables from "./api/tables.route.js";
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/restaurants", resturants)
+app.use("/api/v1/tables", tables)
 app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
 export default app
