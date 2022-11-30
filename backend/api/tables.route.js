910fc0773c0c8ada26b74eed38c36653328c3f07
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2022-11-29 21:11:19 -0600

    Deleting extra files

--- backend/api/tables.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import TablesCtrl from "./tables.controller.js";
-
-const router = express.Router()
-
-router.route("/").get(TablesCtrl.apiGetTables)
-
-export default router
